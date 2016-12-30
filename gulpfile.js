cf3b95e78628c63b7594aac03e92782c9c4f87eb
Nelson <nelsondelega@gmail.com>
2016-12-30 18:10:15 -0200

    criando a stack de desenvolvimento

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const g = require('gulp');
+const HubRegistry = require('gulp-hub');
+const browserSync = require('browser-sync');
+const del = require('del');
+const conf = require('./stack/gulp.conf');
+
+// Load de outros arquivos com tasks do gulp
+const hub = new HubRegistry([conf.path.tasks('gulp_tasks/*.js')]);
+
+
+g.registry(hub);
+
+g.task('default', g.series('build'));
+g.task('build', g.series(g.parallel('clean', 'webpack:dist')));
+
+g.task('serve', g.series('webpack:watch', 'watch', 'browsersync'));
+g.task('serve:dist', g.series('default', 'browsersync:dist'));
+
+g.task('watch', watch);
+g.task('clean', clean);
+
+
+function reloadBrowserSync(cb) {
+  browserSync.reload();
+  cb();
+}
+
+function watch(done) {
+  g.watch(conf.path.tmp('index.html'), reloadBrowserSync);
+  done();
+}
+
+function clean() {
+  return del([conf.paths.dist, conf.paths.tmp]);
+}
