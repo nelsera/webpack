21c94ac65f2e2dacd9b69ae5018044871a8ab353
Nelson <nelsondelega@gmail.com>
2017-01-26 12:05:08 -0200

    melhorias

--- client/feats/establishment/establishment.ctrl.js
+++ client/feats/establishment/establishment.ctrl.js
@@ -3,13 +3,17 @@ import $ from 'jquery';
 export default class Establishment {
   /** @ngInject */
   constructor($rootScope, $rest) {
-    const etb = this;
+    this.xhr = $rest;
+    this.states = $rest.getStates();
+    $rootScope.title = this.title = 'Adicionar estabelecimento';
 
-    etb.rest = $rest;
-    etb.states = $rest.getStates();
-    $rootScope.title = etb.title = 'Adicionar estabelecimento';
+    this.setTab(1);
 
-    etb.setTab(1);
+    this.Infos = {
+      ProductId: 1
+    };
+    this.Address = {};
+    this.Commercial = {};
   }
 
   getDays(day, dayEnd, days = []) {
@@ -23,16 +27,17 @@ export default class Establishment {
     return days;
   }
 
-  getAddress(context) {
-    this.rest.getAddress(this.fields[context].zipcode).then(res => {
+  getAddress(scope) {
+    this.xhr.getAddress(this[scope].Zipcode).then(res => {
       if (res.Street) {
-        this.fields[context].street = res.Street;
-        this.fields[context].district = res.District.Name;
-        this.fields[context].city = res.City.Name;
-        this.fields[context].state = res.State.Code;
-        $(`[ng-model="etb.fields.${context}.number"]`).focus();
+        this[scope] = res;
+        $(`[ng-model="etb.${scope}.Number"]`).focus();
       }
     });
+  }
+
+  formatPhone(e) {
+    return (e) ? {Ddd: e.slice(0, 2), Number: e.slice(2)} : null;
   }
 
   nextStep(isValid, step) {
@@ -43,169 +48,7 @@ export default class Establishment {
     return this.tab = tab;
   }
 
-  setRange() {
-    this.fields.taxiProduct.begin = undefined;
-    this.fields.taxiProduct.end = undefined;
-    this.fields.taxiProduct.close = undefined;
-  }
-
-  signIn() {
-    this.rest.signIn({
-      Id: 0,
-      Name: 'string',
-      Email: 'string',
-      Website: 'string',
-      Trade: 'string',
-      Registration: {
-        City: 'string',
-        State: 'string'
-      },
-      Cnpj: 'string',
-      Address: {
-        Street: 'string',
-        Number: 'string',
-        Complement: 'string',
-        Zipcode: 'string',
-        District: {
-          Id: 0,
-          Name: 'string'
-        },
-        City: {
-          Id: 0,
-          Name: 'string'
-        },
-        State: {
-          Id: 0,
-          Code: 'string'
-        }
-      },
-      Phone: {
-        Ddd: 'string',
-        Number: 'string'
-      },
-      Size: 0,
-      Manager: {
-        Name: 'string',
-        Email: 'string',
-        JobTitle: 'string',
-        Department: 'string',
-        PrivatePhone: {
-          Ddd: 'string',
-          Number: 'string'
-        },
-        CommercialPhone: {
-          Ddd: 'string',
-          Number: 'string'
-        },
-        Address: {
-          Street: 'string',
-          Number: 'string',
-          Complement: 'string',
-          Zipcode: 'string',
-          District: {
-            Id: 0,
-            Name: 'string'
-          },
-          City: {
-            Id: 0,
-            Name: 'string'
-          },
-          State: {
-            Id: 0,
-            Code: 'string'
-          }
-        }
-      },
-      Billing: {
-        ResponsibleOne: {
-          Name: 'string',
-          Email: 'string'
-        },
-        ResponsibleTwo: {
-          Name: 'string',
-          Email: 'string'
-        },
-        Address: {
-          Street: 'string',
-          Number: 'string',
-          Complement: 'string',
-          Zipcode: 'string',
-          District: {
-            Id: 0,
-            Name: 'string'
-          },
-          City: {
-            Id: 0,
-            Name: 'string'
-          },
-          State: {
-            Id: 0,
-            Code: 'string'
-          }
-        }
-      },
-      ShippingAddress: {
-        Street: 'string',
-        Number: 'string',
-        Complement: 'string',
-        Zipcode: 'string',
-        District: {
-          Id: 0,
-          Name: 'string'
-        },
-        City: {
-          Id: 0,
-          Name: 'string'
-        },
-        State: {
-          Id: 0,
-          Code: 'string'
-        }
-      },
-      PaymentSlip: {
-        Charge: true,
-        Send: true,
-        Cost: 0
-      },
-      TaxiProduct: {
-        DeadlineInDays: 0,
-        Contract: 0,
-        Tax: {
-          Type: 0,
-          Value: 0
-        },
-        Period: {
-          Start: 0,
-          End: 0
-        },
-        ClosesInDays: 0,
-        RedundancyDay: 0,
-        Credit: {
-          Limit: 0,
-          Total: 0
-        },
-        Value: 0
-      },
-      Status: {
-        Active: true,
-        Blocked: true
-      }
-    });
-  }
-
-  getFormInfos(data) {
-    const address = (obj = {}) => {
-      return this.fields.formBilling = {
-        zipcode: obj.zipcode,
-        street: obj.street,
-        number: obj.number,
-        complement: obj.complement,
-        district: obj.district,
-        city: obj.city,
-        state: obj.state
-      };
-    };
-
-    return this.switchAddress ? address(data) : address();
+  signIn(Infos) {
+    Infos.ProductId = 1;
   }
 }
